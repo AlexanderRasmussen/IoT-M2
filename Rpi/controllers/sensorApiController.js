876ce7f53c2745b0410be91cbb3a570e61b9aab5
NicklasNielsen <you@example.com>
2018-09-06 12:13:07 +0200

    styled the pages

--- Rpi/controllers/sensorApiController.js
+++ Rpi/controllers/sensorApiController.js
@@ -1,13 +1,13 @@
-var gpio = require('../gpio/gpio');
+//var gpio = require('../gpio/gpio');
 
 module.exports.getHumidity = (req, res) => {
-    res.send({humidity: gpio.humidityRead(), unit: '%'});
+    res.send({humidity: 27, unit: '%'}); //gpio.humidityRead()
 }
 
 module.exports.getTemperature = (req, res) => {
-    res.send({temperature: gpio.temperatureRead(), unit: '°C'});
+    res.send({temperature: 25, unit: '°C'}); //gpio.temperatureRead()
 }
 
 module.exports.getPir = (req, res) => {
-    res.send({pirValue: gpio.pirRead()});
+    res.send({pirValue: 1}); //gpio.pirRead()
 }
--- Rpi/routes/actuatorAPI.js
+++ Rpi/routes/actuatorAPI.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var controller = require('../controllers/actuatorApiController');
 router.get('/toggleLED', controller.ledToggle)
     .get('/turnOnLED', controller.ledTurnOn)
-    .get('/turnOffLED', controller.ledTurnOff);
+    .get('/turnOffLED', controller.ledTurnOff)
+    .get('/LEDstate', controller.ledGetState);
 
 module.exports = router;

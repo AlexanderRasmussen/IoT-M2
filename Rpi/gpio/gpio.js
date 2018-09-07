6ee023c8dd1862d04a4ee8111caa8b39e69a4227
Anders Meidahl <Andersmeidahl@gmail.com>
2018-09-07 11:03:56 +0200

    Commented gpio in

--- Rpi/gpio/gpio.js
+++ Rpi/gpio/gpio.js
@@ -1,57 +1,57 @@
-// var Gpio = require('onoff').Gpio;
-// var DhtSensor = require('rpi-dht-sensor');
-// var dht = new DhtSensor.DHT22(12);
-// var ledstate = false;
-// var pirval = 'Waiting for update';
+var Gpio = require('onoff').Gpio;
+var DhtSensor = require('rpi-dht-sensor');
+var dht = new DhtSensor.DHT22(12);
+var ledstate = false;
+var pirval = 'Waiting for update';
 
-// module.exports.ledOn = () => {
-//     var led = new Gpio(4, 'out');
-//     led.writeSync(1);
-// }
+module.exports.ledOn = () => {
+    var led = new Gpio(4, 'out');
+    led.writeSync(1);
+}
 
-// module.exports.ledOff = () => {
-//     var led = new Gpio(4, 'out');
-//     led.writeSync(0);
-// }
+module.exports.ledOff = () => {
+    var led = new Gpio(4, 'out');
+    led.writeSync(0);
+}
 
-// module.exports.ledToggle = () => {
-//     var led = new Gpio(4, 'out');
+module.exports.ledToggle = () => {
+    var led = new Gpio(4, 'out');
 
-//     if(ledstate == false){
-//         ledstate = true;
-//         led.writeSync(1);
-//     }
-//     else{
-//         ledstate = false;
-//         led.writeSync(0);
-//     }
-// }
+    if(ledstate == false){
+        ledstate = true;
+        led.writeSync(1);
+    }
+    else{
+        ledstate = false;
+        led.writeSync(0);
+    }
+}
 
-// module.exports.ledGetState = () => {
-//     return ledstate;
-// }
+module.exports.ledGetState = () => {
+    return ledstate;
+}
 
-// module.exports.humidityRead = () => {
-//     var readout = dht.read();
-//     console.log('Humidity: ' + readout.humidity.toFixed(2));
-//     return readout.humidity.toFixed(2);
-// }
+module.exports.humidityRead = () => {
+    var readout = dht.read();
+    console.log('Humidity: ' + readout.humidity.toFixed(2));
+    return readout.humidity.toFixed(2);
+}
 
-// module.exports.temperatureRead = () => {
-//     var readout = dht.read();
-//     console.log('Temp: ' + readout.temperature.toFixed(2));
-//     return readout.temperature.toFixed(2);
-// }
+module.exports.temperatureRead = () => {
+    var readout = dht.read();
+    console.log('Temp: ' + readout.temperature.toFixed(2));
+    return readout.temperature.toFixed(2);
+}
 
-// module.exports.pirRead = () => {
-//     var pir = new Gpio(17, 'in', 'both');
+module.exports.pirRead = () => {
+    var pir = new Gpio(17, 'in', 'both');
     
-//     pir.watch((err, val) => {
-//         if(err) throw err;
+    pir.watch((err, val) => {
+        if(err) throw err;
         
-//         console.log('Pir: ' + val);
-//         pirval = val.toString();
-//     })
+        console.log('Pir: ' + val);
+        pirval = val.toString();
+    })
 
-//     return pirval;
-// }
+    return pirval;
+}
